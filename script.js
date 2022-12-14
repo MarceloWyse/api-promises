fe6313393d176c08449cca423557fbd8d019f272
MarceloWyse <marceloamaral88@gmail.com>
2022-12-14 18:52:43 -0300

    modificacoes

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 async function buscaEndereco(cep) {
+    var msgErro = document.getElementById('erro');
+    msgErro.innerHTML = "";
     try {
     var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCepConvertido = await consultaCep.json();
     if(consultaCepConvertido.erro){
         throw Error('Cep não existente');
     }
-    console.log(consultaCepConvertido); 
-    return consultaCepConvertido;   
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+    
+    cidade.value = consultaCepConvertido.localidade;
+    logradouro.value = consultaCepConvertido.logradouro;
+    estado.value = consultaCepConvertido.uf;
+
+    //console.log(consultaCepConvertido); 
+    return consultaCepConvertido;  
+    
     }
     catch (e) {
-        console.log(e);
+        msgErro.innerHTML = "Cep inválido";
     }
 }
 
-let ceps = ['96015710','01001000']; 
+/* let ceps = ['96015710','01001000']; 
 let conjuntoCeps = ceps.map(e => buscaEndereco(e)) //map() retorna um array, nesse caso de promises
 Promise.all(conjuntoCeps).then(e => console.log(e)); //resolve o array de promises
-
-
-
+ */
+var cep = document.getElementById("cep");
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
